548d5cb6d95e2ac8fbbac82d24aff5e8e2c61f22
adrisala20 <adriana_15_tuya@hotmail.com>
2024-05-20 16:57:22 +0200

    added header

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+
+Route::get('/characters', function () {
+    return view('characters');
+})->name('characters');
+
+Route::get('/comics', function () {
+    return view('comics');
+})->name('comics');
+
+Route::get('/movies', function () {
+    return view('movies');
+})->name('movies');
+
+Route::get('/tv', function () {
+    return view('tv');
+})->name('tv');
+
+Route::get('/games', function () {
+    return view('games');
+})->name('games');
+
+Route::get('/collectibles', function () {
+    return view('collectibles');
+})->name('collectibles');
+
+Route::get('/videos', function () {
+    return view('videos');
+})->name('videos');
+
+Route::get('/fans', function () {
+    return view('fans');
+})->name('fans');
+
+Route::get('/new', function () {
+    return view('new');
+})->name('new');
+
+Route::get('/shops', function () {
+    return view('shops');
+})->name('shops');
